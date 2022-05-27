6ac6e12587be575211575e3ed494451649e0b600
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-05-27 09:03:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
+        if(!num)
+            return 0;
         int count=0;
         while(num)
         {
             if(num%2)
-                num--;
-            else
-                num/=2;
+                count++;
+            num/=2;
             count++;
         }
-        return count;
+        return count-1;
     }
 };
