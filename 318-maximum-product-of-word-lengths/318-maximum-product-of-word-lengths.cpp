32539d1ec51fcc4ed8cf4993efb77e9de8872ee5
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-05-29 10:53:40 +0530

    Time: 58 ms (73.14%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -3,21 +3,18 @@ public:
     int maxProduct(vector<string>& words) 
     {
         int ans=0,n=words.size();
-        vector<int> v;
-        int bit=0;
-        for(string &s:words)
-        {
-            bit=0;
-            for(char &c:s)
-            {
-                bit|=(1<<(c-'a'));
-            }
-            v.push_back(bit);
-        }
+        vector<int> v(n);
+        int mask=0;
         bool poss;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            mask=0;
+            for(char &c:words[i])
+            {
+                mask|=(1<<(c-'a'));
+            }
+            v[i]=mask;
+            for(int j=0;j<i;j++)
             {
                 int len=words[i].length()*words[j].length();
                 if((v[i]&v[j])==0)
