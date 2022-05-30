b4616a15c9497b2822599677fdab7618d41e77a1
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-05-30 09:56:32 +0530

    Time: 600 ms (48.45%), Space: 148.6 MB (71.55%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
@@ -1,22 +1,59 @@
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-typedef tree<pair<int,string>, null_type, less<pair<int,string>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 class SORTracker {
+private:
+    bool check(const pair<int,string> &a,const pair<int,string> &b)
+    {
+        if(a.first*(-1)>b.first*(-1))
+            return true;
+        else if(a.first*(-1)<b.first*(-1))
+            return false;
+        else if(a.second<b.second)
+            return true;
+        return false;
+    }
 public:
-    pbds s;
-    int cnt=0;
+    priority_queue<pair<int,string>> large;
+    priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>> small;
+    int k=0;
     SORTracker() {
         
     }
-    void add(string name, int score) {
-        s.insert({-score,name});
+    void add(string name, int score)
+    {
+        auto p=make_pair(-score,name);
+        if(k>=large.size())
+        {
+            if(small.size()>0 and check(small.top(),p))
+            {
+                large.push(small.top());
+                small.pop();
+                small.push(p);
+            }
+            else
+                large.push(p);
+        }
+        else
+        {
+            if(check(p,large.top()))
+            {
+                small.push(large.top());
+                large.pop();
+                large.push(p);
+            }
+            else
+                small.push(p);
+        }
     }
     
-    string get() {
-        auto res=s.find_by_order(cnt);
-        cnt++;
-        return res->second;
+    string get() 
+    {
+        k++;
+        string res=large.top().second;
+        if(large.size()<=k and small.size())
+        {
+            large.push(small.top());
+            small.pop();
+        }
+        return res;
     }
 };
 
