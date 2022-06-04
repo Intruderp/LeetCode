de3d476536cac54cd17ec45f4d816b01fbdf77f6
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-04 20:08:51 +0530

    Time: 89 ms (70.17%), Space: 27.8 MB (58.45%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        map<pair<int,int>,int> m;
+        map<int,int> m;
         int n=intervals.size();
         for(int i=0;i<n;i++)
         {
-            m[{intervals[i][0],intervals[i][1]}]=i;
+            m[intervals[i][0]]=i;
         }
         vector<int> ans(n);
         for(int i=0;i<n;i++)
         {
-            auto it=m.lower_bound({intervals[i][1],INT_MIN});
+            auto it=m.lower_bound(intervals[i][1]);
             if(it!=m.end())
                 ans[i]=it->second;
             else
