da2be2105f8649f94e0b2488468e516f9c7b9d36
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-07 14:33:57 +0530

    Time: 100 ms (36.17%), Space: 38.5 MB (28.64%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
         int res=0,lastOne=0,n=flips.size(),steps=0;
-        set<int> zero;
-        for(int i=1;i<=n;i++)
-            zero.insert(i);
         for(auto &flip:flips)
         {
             steps++;
-            if(steps==n)
-                break;
             lastOne=max(flip,lastOne);
-            zero.erase(flip);
-            if(lastOne==steps and *zero.begin()==steps+1)
+            if(lastOne==steps)
                 res++;
         }
-        return res+1;
+        return res;
     }
 };
