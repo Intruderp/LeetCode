bc07049b3e9bd56d4e5abc80ea71a37fa80e98f1
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-14 08:55:09 +0530

    Time: 52 ms (21.00%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int n,m,dp[500][500];
-    int help(int i,int j,string &x,string &y)
-    {
-        if(i==n)
-            return m-j;
-        if(j==m)
-            return n-i;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(x[i]==y[j])
-            return dp[i][j]=help(i+1,j+1,x,y);
-        else 
-            return dp[i][j]=min(help(i+1,j,x,y),help(i,j+1,x,y))+1;
-    }
     int minDistance(string word1, string word2) {
-        n=word1.length();
-        m=word2.length();
-        memset(dp,-1,sizeof(dp));
-        return help(0,0,word1,word2);
+        int n=word1.length(),m=word2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+            dp[i][0]=i;
+        for(int j=1;j<=m;j++)
+            dp[0][j]=j;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+            }
+        }
+        return dp[n][m];
     }
 };
