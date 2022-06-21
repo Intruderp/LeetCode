e2910264910075699f5837efd1d120c7a76ae4bd
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-21 08:57:35 +0530

    Time: 183 ms (31.19%), Space: 53.6 MB (77.22%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-    bool check(vector<int> &heights,int bricks,int ladders,int x)
-    {
-        priority_queue<int> q;
-        for(int i=1;i<=x;i++)
-        {
-            if(heights[i]>heights[i-1])
-            {
-                q.push(heights[i]-heights[i-1]);
-            }
-        }
-        while(q.size())
-        {
-            if(ladders)
-                ladders--;
-            else
-                bricks-=q.top();
-            q.pop();
-            if(bricks<0)
-                return false;
-        }
-        return true;
-    }
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n=heights.size(),low=1,mid,high=n-1,ans=0;
-        while(low<=high)
+        long long sum=0;
+        priority_queue<int> q;
+        int n=heights.size(),i=1,diff;
+        for(;i<n;i++)
         {
-            mid=(high+low)/2;
-            if(check(heights,bricks,ladders,mid))
+            diff=heights[i]-heights[i-1];
+            if(diff>0)
             {
-                ans=mid;
-                low=mid+1;
+                if(q.size()<ladders)
+                    q.push(-diff);
+                else
+                {
+                    if(q.size()==0)
+                    {
+                        if(diff>bricks)
+                            break;
+                        bricks-=diff;
+                    }
+                    else if(q.top()*(-1)<diff)
+                    {
+                        bricks-=(q.top()*(-1));
+                        if(bricks<0)
+                            break;
+                        q.pop();
+                        q.push(-diff);
+                    }
+                    else
+                    {
+                        if(diff>bricks)
+                            break;
+                        bricks-=diff;
+                    }
+                }
+                    
             }
-            else
-                high=mid-1;
         }
-        return ans;
+        return i-1;
     }
 };
