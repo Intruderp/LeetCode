d82f446cfbb27431afb82b081862a1d9da9f7252
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-25 06:55:36 +0530

    Time: 61 ms (10.83%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    bool check(vector<int> &nums)
-    {
-        int n=nums.size();
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i+1]<nums[i])
-                return false;
-        }
-        return true;
-    }
     bool checkPossibility(vector<int>& nums) {
         int cnt=0,n=nums.size();
         for(int i=0;i<n-1;i++)
         {
             if(nums[i+1]<nums[i])
             {
-                int temp=nums[i];
-                nums[i]=nums[i+1];
-                bool ans=check(nums);
-                nums[i]=temp;
-                nums[i+1]=nums[i];
-                ans|=check(nums);
-                return ans;
+                if(i-1>=0 and nums[i-1]>nums[i+1])
+                    nums[i+1]=nums[i];
+                else
+                    nums[i]=nums[i+1];
+                cnt++;
             }
         }
-        return true;
+        return cnt<=1;
     }
 };
