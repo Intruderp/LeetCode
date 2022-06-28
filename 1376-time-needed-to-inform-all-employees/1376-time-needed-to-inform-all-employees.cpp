0492dbc449a2adb7707d7d7effec5ade53199666
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-28 18:10:20 +0530

    Time: 477 ms (53.37%), Space: 112.8 MB (79.54%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
+    int dfs(int src,vector<int> &informTime,vector<int> adj[],vector<int>& manager)
+    {
+        int ans=0;
+        for(auto &child:adj[src])
+        {
+            ans=max(ans,dfs(child,informTime,adj,manager)+informTime[manager[child]]);
+        }
+        return ans;
+    }
     int numOfMinutes(int n, int headId, vector<int>& manager, vector<int>& informTime) 
     {
         vector<int> adj[n];
-        vector<int> dist(n,INT_MAX);
         for(int i=0;i<n;i++){
             if(i==headId)
                 continue;
             adj[manager[i]].push_back(i);
         }
-        int res=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        q.push({0,headId});
-        dist[headId]=0;
-        while(!q.empty()){
-            auto t=q.top();
-            q.pop();
-            if(dist[t.second]<t.first)
-                continue;
-            for(auto &child:adj[t.second])
-            {
-                if(dist[child]>t.first+informTime[manager[child]])
-                {
-                    dist[child]=t.first+informTime[manager[child]];
-                    q.push({dist[child],child});
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            res=max(res,dist[i]);
-        }
-        return res;
+        return dfs(headId,informTime,adj,manager);
     }
 };
