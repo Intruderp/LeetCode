2dface4b1014b84156a5c3831e068165c99a2c03
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-28 18:03:56 +0530

    Time: 833 ms (15.55%), Space: 114.7 MB (78.39%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int numOfMinutes(int n, int headId, vector<int>& manager, vector<int>& informTime) 
     {
-        vector<pair<int,int>> adj[n];
+        vector<int> adj[n];
         vector<int> dist(n,INT_MAX);
         for(int i=0;i<n;i++){
             if(i==headId)
                 continue;
-            adj[manager[i]].push_back({i,informTime[manager[i]]});
+            adj[manager[i]].push_back(i);
         }
         int res=0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
@@ -18,10 +18,10 @@ public:
             q.pop();
             for(auto &child:adj[t.second])
             {
-                if(dist[child.first]>t.first+child.second)
+                if(dist[child]>t.first+informTime[manager[child]])
                 {
-                    dist[child.first]=t.first+child.second;
-                    q.push({dist[child.first],child.first});
+                    dist[child]=t.first+informTime[manager[child]];
+                    q.push({dist[child],child});
                 }
             }
         }
