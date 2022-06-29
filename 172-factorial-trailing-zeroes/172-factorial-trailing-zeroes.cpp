a87b8588d0f224f3af7561faa704cc8adb892246
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-29 14:46:33 +0530

    Time: 8 ms (12.36%), Space: 5.9 MB (74.25%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int cnt2=0,cnt5=0;
-        int x=n,p=2;
-        while(x/p)
+        int res=0,m=5;
+        while(n/m)
         {
-            cnt2+=x/p;
-            p*=2;
+            res+=n/m;
+            m*=5;
         }
-        p=5;
-        while(n/p)
-        {
-            cnt5+=n/p;
-            p*=5;
-        }
-        return min(cnt2,cnt5);
+        return res;
     }
 };
