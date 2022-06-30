2d9ee781293bc93692fee0d22aa4a22dd584e04c
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-06-30 17:04:14 +0530

    Time: 53 ms (27.67%), Space: 10.1 MB (69.89%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int n=answerKey.length(),res=0,x,y,sz,sz2;
-        vector<int> t,f;
-        for(int i=n-1;i>=0;i--)
+    int help(string &s,int k,char c)
+    {
+        int n=s.length(),i=0,j=0,res=0;
+        while(j<n)
         {
-            if(answerKey[i]=='T')
-                t.push_back(i);
-            else
-                f.push_back(i);
-        }
-        for(int i=0;i<n;i++)
-        {
-            sz=t.size();
-            sz2=f.size();
-            if(sz-k-1<0)
-                res=max(res,n-i);
-            else
-                res=max(res,t[sz-k-1]-i);
-            if(sz2-k-1<0)
-                res=max(res,n-i);
-            else
-                res=max(res,f[sz2-k-1]-i);
-            if(t.size() and t.back()<=i)
-                t.pop_back();
-            if(f.size() and f.back()<=i)
-                f.pop_back();
-            
+            while(j<n and k)
+            {
+                if(s[j]==c)
+                    k--;
+               j++;
+            }
+            while(j<n and s[j]!=c)
+                j++;
+            res=max(res,j-i);
+            if(s[i]==c)
+                k++;
+            i++;
         }
         return res;
+    }
+    int maxConsecutiveAnswers(string answerKey, int k) {
+       return max(help(answerKey,k,'T'),help(answerKey,k,'F'));
     }
 };
