8770ab954e16e4110fd60d6a8bf7f1320d8ea4e4
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-04 15:49:28 +0530

    Time: 91 ms (98.06%), Space: 24.4 MB (72.98%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    long long dp[100000][4][3];
-    long long help(int i,string &s,int cnt,int p)
-    {
-        if(cnt>3)
-            return 0;
-        if(i==s.length())
-            return cnt==3;
-        if(dp[i][cnt][p]!=-1)
-            return dp[i][cnt][p];
-        if((s[i]-'0')!=p)
-            return dp[i][cnt][p]=help(i+1,s,cnt+1,s[i]-'0')+help(i+1,s,cnt,p);
-        else
-            return dp[i][cnt][p]=help(i+1,s,cnt,p);
-    }
     long long numberOfWays(string s) {
-        memset(dp,-1,sizeof(dp));
-        return  help(0,s,0,2);
+        int n=s.length(),countOne=0,countZero=0;
+        long long res=0,resOne=0,resZero=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]=='1')
+            {
+                res+=resZero;
+                resOne+=countZero;
+                countOne++;
+            }
+            else
+            {
+                res+=resOne;
+                resZero+=countOne;
+                countZero++;
+            }
+        }
+        return res;
     }
 };
