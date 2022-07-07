37ec9eb051f5016c3dd9aee97238d9462c18892d
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-07 08:25:52 +0530

    Time: 5 ms (36.76%), Space: 6.1 MB (89.31%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        int freq[26]={0},n=s.length(),mx=0;
+        int freq[26]={0},n=s.length(),mx=0,ind=0;
+        char m;
         for(char &c:s)
         {
             freq[c-'a']++;
-            mx=max(mx,freq[c-'a']);
+            if(freq[c-'a']>mx)
+            {
+                mx=freq[c-'a'];
+                m=c;
+            }
         }
         if(mx>(n+1)/2)
             return "";
-        string res;
-        priority_queue<pair<int,char>> q;
+        string res=s;
+        while(freq[m-'a'])
+        {
+            res[ind]=m;
+            ind+=2;
+            freq[m-'a']--;
+        }
         for(int i=0;i<26;i++)
         {
-            if(freq[i])
-                q.push({freq[i],'a'+i});
-        }
-        while(!q.empty())
-        {
-            auto t=q.top();
-            q.pop();
-            if(res.length()==0 or res.back()!=t.second)
+            while(freq[i]--)
             {
-                res.push_back(t.second);
-                if(t.first>1)
-                    q.push({t.first-1,t.second});
-            }
-            else
-            {
-                auto t2=q.top();
-                q.pop();
-                res.push_back(t2.second);
-                if(t2.first>1)
-                    q.push({t2.first-1,t2.second});
-                q.push(t);
+                if(ind>=n)
+                    ind=1;
+                res[ind]=(i+'a');
+                ind+=2;
             }
         }
         return res;
