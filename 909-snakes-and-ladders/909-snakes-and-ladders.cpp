4b4abaa394116d23a3afecd56caf95f9d2e8bfe1
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-08 09:33:53 +0530

    Time: 46 ms (32.14%), Space: 12.7 MB (80.80%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.cpp
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n=board.size(),row,col,num,newJ;
-        vector<int> dist(n*n+1,INT_MAX);
-        queue<pair<int,int>> q;
-        q.push({0,1});
+        vector<int> dist(n*n+1,-1);
+        queue<int> q;
+        q.push(1);
+        dist[1]=0;
         while(!q.empty())
         {
             auto t=q.front();
+            if(t==n*n)
+                return dist[n*n];
             q.pop();
-            if(dist[t.second]<t.first)
-                continue;
-            for(int j=t.second+1;j<=min(n*n,t.second+6);j++)
+            for(int j=t+1;j<=min(n*n,t+6);j++)
             {
                 row=(j-1)/n;
                 if(row%2)
@@ -26,23 +27,23 @@ public:
                 }
                 if(board[n-row-1][col]==-1)
                 {
-                    if(dist[j]>t.first+1)
+                    if(dist[j]==-1)
                     {
-                        dist[j]=t.first+1;
-                        q.push({dist[j],j});
+                        dist[j]=dist[t]+1;
+                        q.push(j);
                     }
                 }
                 else
                 {
                     newJ=board[n-row-1][col];
-                    if(dist[newJ]>t.first+1)
+                    if(dist[newJ]==-1)
                     {
-                        dist[newJ]=t.first+1;
-                        q.push({dist[newJ],newJ});
+                        dist[newJ]=dist[t]+1;
+                        q.push(newJ);
                     }
                 }
             }
         }
-        return dist[n*n]==INT_MAX ? -1 : dist[n*n];
+        return dist[n*n];
     }
 };
