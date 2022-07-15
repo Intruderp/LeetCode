1797bcc3b427d807e566b46c4a23e72b80c52c78
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-15 10:21:12 +0530

    Time: 563 ms (17.95%), Space: 116 MB (20.87%) - LeetHub

---
+++ 2333-minimum-sum-of-squared-difference/2333-minimum-sum-of-squared-difference.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k1, int k2)
+    {
+        unordered_map<int,int> diff;
+        int n=nums1.size(),d=0;
+        long long res=0,x;
+        for(int i=0;i<n;i++)
+        {
+            diff[abs(nums1[i]-nums2[i])]++;
+            d=max(d,abs(nums1[i]-nums2[i]));
+        }
+        k1+=k2;
+        while(d and k1)
+        {
+            if(diff.count(d)==0)
+            {
+                d--;
+                continue;
+            }
+            if(diff[d]>k1)
+            {
+                diff[d]-=k1;
+                diff[d-1]+=k1;
+                k1=0;
+            }
+            else
+            {
+                k1-=diff[d];
+                diff[d-1]+=diff[d];
+                diff.erase(d);
+            }
+            d--;
+        }
+        for(auto &it:diff)
+        {
+            x=it.first;
+            res+=x*x*it.second;
+        }
+        return res;
+    }
+};
