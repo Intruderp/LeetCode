9c57fd00ac6a3202fb789dd0f2c0df42fa39b06f
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-16 15:49:53 +0530

    Time: 773 ms (32.93%), Space: 8.7 MB (67.07%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int res=0,mx,mn,n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            vector<int> freq(26,0);
+            for(int j=i;j<n;j++)
+            {
+                freq[s[j]-'a']++;
+                mx=0,mn=n;
+                for(int c=0;c<26;c++)
+                {
+                    if(freq[c])
+                    {
+                        mx=max(mx,freq[c]);
+                        mn=min(mn,freq[c]);
+                    }
+                }
+                res+=mx-mn;
+            }
+        }
+        return res;
+    }
+};
