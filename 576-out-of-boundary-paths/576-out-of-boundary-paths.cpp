dd8a796167093c99ccbc22cc32e6509538c65572
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-16 09:16:30 +0530

    Time: 26 ms (28.70%), Space: 6.6 MB (71.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     const int mod=1e9+7;
     int dp[50][50][51];
-    long long help(int i,int j,int m,int n,int maxMove)
+    bool isValid(int i,int j,int m,int n)
     {
-        if(i<0 or j<0 or i>=m or j>=n)
-            return 1;
-        if(maxMove==0)
-            return 0;
-        if(dp[i][j][maxMove]!=-1)
-            return dp[i][j][maxMove];
-        int count=(help(i+1,j,m,n,maxMove-1)+help(i-1,j,m,n,maxMove-1))%mod;
-        count=(count+help(i,j+1,m,n,maxMove-1))%mod;
-        count=(count+help(i,j-1,m,n,maxMove-1))%mod;
-        return dp[i][j][maxMove]=count;
+        return i>=0 and j>=0 and i<m and j<n;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        memset(dp,-1,sizeof(dp));
-        return help(startRow,startColumn,m,n,maxMove);
+        int dr[]={0,1,-1,0};
+        int dc[]={1,0,0,-1};
+        int ni,nj;
+        for(int moves=1;moves<=maxMove;moves++)
+        {
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    for(int d=0;d<4;d++)
+                    {
+                        ni=i+dr[d];
+                        nj=j+dc[d];
+                        if(isValid(ni,nj,m,n))
+                            dp[i][j][moves]+=dp[ni][nj][moves-1];
+                        else
+                            dp[i][j][moves]++;
+                        dp[i][j][moves]%=mod;
+                    }
+                }
+            }
+        }
+        return dp[startRow][startColumn][maxMove];
     }
 };
