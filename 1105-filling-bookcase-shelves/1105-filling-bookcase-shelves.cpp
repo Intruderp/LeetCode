cf3a4f311a501fb29ae7cd18b5f0ec7fa9fbecb4
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-19 15:26:34 +0530

    Time: 8 ms (69.66%), Space: 8.1 MB (95.05%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int n,dp[1000];
-    int help(int i,int shelfWidth,vector<vector<int>> &books)
-    {
-        if(i==n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int ans=INT_MAX,cur=0,mx=0;
-        for(int j=i;j<n;j++)
-        {
-            cur+=books[j][0];
-            mx=max(mx,books[j][1]);
-            if(cur>shelfWidth)
-                break;
-            ans=min(ans,help(j+1,shelfWidth,books)+mx);
-        }
-        return dp[i]=ans;
-    }
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) 
     {
-        n=books.size();
-        memset(dp,-1,sizeof(dp));
-        return help(0,shelfWidth,books);
+        int n=books.size(),cur,mx;
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            cur=0,mx=0;
+            for(int j=i;j>=1;j--)
+            {
+                cur+=books[j-1][0];
+                mx=max(mx,books[j-1][1]);
+                if(cur>shelfWidth)
+                    break;
+                dp[i]=min(dp[i],dp[j-1]+mx);
+            }
+        }
+        return dp[n];
     }
 };
