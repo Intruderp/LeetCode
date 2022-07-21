60f370661016c1d270d33016204fe4e895035ddc
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-21 13:02:57 +0530

    Time: 148 ms (12.78%), Space: 8.4 MB (57.14%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     const int mod=1e9+7;
-    int dp[30][1001];
-    int help(int i,int n,int k,int target)
-    {
-        if(target<0)
-            return 0;
-        if(i==n)
-            return target==0;
-        if(dp[i][target]!=-1)
-            return dp[i][target];
-        int ans=0;
-        for(int num=1;num<=k;num++)
-            ans=(ans+help(i+1,n,k,target-num))%mod;
-        return dp[i][target]=ans;
-            
-    }
     int numRollsToTarget(int n, int k, int target) {
-        memset(dp,-1,sizeof(dp));
-        return help(0,n,k,target);
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                for(int num=1;num<=min(k,j);num++)
+                    dp[i][j]=(dp[i][j]+dp[i-1][j-num])%mod;
+            }
+        }
+        return dp[n][target];
     }
 };
