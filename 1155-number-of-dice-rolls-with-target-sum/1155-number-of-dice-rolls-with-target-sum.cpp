bab7a4097e9bf0ba9e8aac80db02a3656f84cbac
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-21 13:27:20 +0530

    Time: 35 ms (67.73%), Space: 8.5 MB (27.60%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,12 +4,25 @@ public:
     int numRollsToTarget(int n, int k, int target) {
         vector<vector<int>> dp(n+1,vector<int>(target+1,0));
         dp[0][0]=1;
+        int num,sum;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=target;j++)
+                dp[i-1][j]=(dp[i-1][j]+dp[i-1][j-1])%mod;
+            for(int j=1;j<=target;j++)
             {
-                for(int num=1;num<=min(k,j);num++)
-                    dp[i][j]=(dp[i][j]+dp[i-1][j-num])%mod;
+                if(i==1)
+                {
+                    if(j<=k)
+                        dp[i][j]=1;
+                }
+                else
+                {
+                    if(j<=k)
+                        dp[i][j]=dp[i-1][j-1];
+                    else
+                        dp[i][j]=(dp[i-1][j-1]-dp[i-1][j-k-1]+mod)%mod;
+                }
             }
         }
         return dp[n][target];
