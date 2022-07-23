f11a9e07715723834ca359092a56ad281c31f120
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-07-23 10:45:53 +0530

    Time: 155 ms (88.63%), Space: 87 MB (5.00%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
-    void merge(int l,int mid,int r,vector<int> &nums,int &inv)
-    {
-        int l_sz=mid-l+1,r_sz=r-mid,L[l_sz+1],R[r_sz+1],j=0,k=0;
-        for(int i=0;i<l_sz;i++)
-        {
-            L[i]=nums[i+l];
-        }
-        for(int i=0;i<r_sz;i++)
-        {
-            R[i]=nums[mid+1+i];
-        }
-        L[l_sz]=R[r_sz]=INT_MAX;
-        for(int i=l;i<=r;i++)
-        {
-            if(R[k]<L[j])
-            {
-                nums[i]=R[k];
-                inv+=l_sz-j;
-                if(inv>nums.size())
-                    return ;
-                k++;
-            }
-            else
-            {
-                nums[i]=L[j];
-                j++;
-            }
-        }
-    }
-    void mergeSort(int l,int r,vector<int> &nums,int &inv)
-    {
-        if(l>=r or inv>nums.size())
-            return ;
-        int mid=(l+r)/2;
-        mergeSort(l,mid,nums,inv);
-        mergeSort(mid+1,r,nums,inv);
-        merge(l,mid,r,nums,inv);
-    }
     bool isIdealPermutation(vector<int>& nums) {
-        int localInv=0,n=nums.size(),globalInv=0;
-        for(int i=0;i<n-1;i++)
-            localInv+=nums[i]>nums[i+1];
-        mergeSort(0,n-1,nums,globalInv);
+        int localInv=0,n=nums.size(),globalInv=0,cnt=0;
+        vector<int> pre(n,0);
+        for(int i=0;i<n;i++)
+        {
+            cnt+=pre[i];
+            if(i+1<n)
+                localInv+=nums[i]>nums[i+1];
+            globalInv+=max(0,nums[i]-cnt);
+            if(globalInv>n)
+                break;
+            pre[nums[i]]+=1;
+            if(i>=nums[i])
+                cnt++;
+        }
         return globalInv==localInv;
     }
 };
