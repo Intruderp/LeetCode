883d1d42fda551a27828a46375226c36dbd1bb30
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-01 16:09:44 +0530

    Time: 407 ms (68.05%), Space: 88.7 MB (98.85%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) 
     {
-        priority_queue<pair<int,int>> pq;
+        deque<int> q;
         int res=INT_MIN,j=1,n=points.size();
-        unordered_set<int> s;
         for(int i=0;i<n;i++)
         {
             if(j<=i)
                 j=i+1;
             while(j<n and points[j][0]-points[i][0]<=k)
             {
-                pq.push({points[j][0]+points[j][1],j});
+                while(!q.empty() and points[q.back()][0]+points[q.back()][1]<=points[j][0]+points[j][1])
+                    q.pop_back();
+                q.push_back(j);
                 j++;
             }
-            while(pq.size() and pq.top().second<=i)
-                pq.pop();
-            if(pq.size())
-                res=max(res,points[i][1]-points[i][0]+pq.top().first);
+            while(q.size() and q.front()<=i)
+                q.pop_front();
+            if(q.size())
+                res=max(res,points[i][1]-points[i][0]+points[q.front()][0]+points[q.front()][1]);
         }
         return res;
     }
