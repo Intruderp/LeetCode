fc97dc6d3408f46937972d9c088cd2b307ae7090
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-04 18:18:07 +0530

    Time: 198 ms (69.13%), Space: 49.9 MB (90.91%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -3,15 +3,21 @@ public:
     int longestSubarray(vector<int>& nums, int limit) 
     {
         int n=nums.size(),i=0,j=0,res=0;
-        map<int,int> map;
+        deque<int> q,q2;
         while(j<n)
         {
-            map[nums[j++]]++;
-            while(map.rbegin()->first-map.begin()->first>limit)
+            while(!q.empty() and nums[q.back()]<=nums[j])
+                q.pop_back();
+            while(!q2.empty() and nums[q2.back()]>=nums[j])
+                q2.pop_back();
+            q.push_back(j);
+            q2.push_back(j++);
+            while(nums[q.front()]-nums[q2.front()]>limit)
             {
-                map[nums[i]]--;
-                if(map[nums[i]]==0)
-                    map.erase(nums[i]);
+                if(q.front()<=i)
+                    q.pop_front();
+                if(q2.front()<=i)
+                    q2.pop_front();
                 i++;
             }
             res=max(res,j-i);
