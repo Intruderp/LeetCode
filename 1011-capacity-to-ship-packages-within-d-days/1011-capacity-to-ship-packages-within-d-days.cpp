40de1f80c29a5907500be414d70ab3e107f10c40
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-06 12:42:36 +0530

    Time: 93 ms (47.73%), Space: 26 MB (93.65%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private: 
+    bool check(vector<int> &weights,int x,int n,int days)
+    {
+        int cur=0,req=0;
+        for(int i=0;i<n;i++)
+        {
+            if(cur+weights[i]<=x)
+            {
+                cur+=weights[i];
+            }
+            else
+            {
+                req++;
+                cur=weights[i];
+            }
+            if(req>days)
+                return false;
+        }
+        if(cur)
+            req++;
+        return req<=days;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) 
+    {
+        int n=weights.size(),low=*max_element(weights.begin(),weights.end()),high=n*500,mid,res=-1;
+        while(low<=high)
+        {
+            mid=(high+low)/2;
+            if(check(weights,mid,n,days))
+            {
+                res=mid;
+                high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return res;
+    }
+};
