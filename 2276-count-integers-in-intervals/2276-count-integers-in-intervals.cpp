8d4d16eb9e1be6f7e508d7eb68805b78e5b546c9
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-09 19:28:07 +0530

    Time: 829 ms (40.89%), Space: 188.2 MB (15.01%) - LeetHub

---
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.cpp
@@ -0,0 +1,44 @@
+class CountIntervals {
+public:
+    int res=0;
+    set<pair<int,int>> s;
+    CountIntervals()
+    {
+        
+    }
+    
+    void add(int left, int right) 
+    {
+        int L=left;
+        pair<int,int> p={left,0};
+        auto it=s.lower_bound(p);
+        set<pair<int,int>> temp;
+        while(it!=s.end())
+        {
+            if(it->second>right)
+                break;
+            temp.insert(*it);
+            right=max(right,it->first);
+            L=min(L,it->second);
+            it++;
+        }
+        for(auto &it:temp)
+        {
+            s.erase(it);
+            res-=it.first-it.second+1;
+        }
+        res+=right-L+1;
+        s.insert({right,L});
+    }
+    
+    int count() {
+        return res;
+    }
+};
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * CountIntervals* obj = new CountIntervals();
+ * obj->add(left,right);
+ * int param_2 = obj->count();
+ */
