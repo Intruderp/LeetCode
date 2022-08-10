7961ce0705d0abb1f600a407c643c461c6a63520
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-10 16:57:08 +0530

    Time: 6 ms (56.82%), Space: 7.7 MB (29.27%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -3,13 +3,14 @@ public:
     int countTriplets(vector<int>& arr) {
         unordered_map<int,int> freq,m;
         freq[0]=1;
+        m[0]=-1;
         int n=arr.size(),res=0,x=0;
         for(int i=0;i<n;i++)
         {
             x^=arr[i];
-            res+=freq[x]*(i)-m[x];
+            res+=freq[x]*(i-1)-m[x];
             freq[x]++;
-            m[x]+=i+1;
+            m[x]+=i;
         }
         return res;
     }
