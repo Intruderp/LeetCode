ffb7a60cf4534cb8fb2e25c8b2965c9224015315
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-11 11:35:15 +0530

    Time: 26 ms (76.72%), Space: 7.1 MB (83.99%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[300][26][26];
+    int dist(int x,int y)
+    {
+        int r1=(x+6)/6,r2=(y+6)/6,c1=x-(x/6)*6+1,c2=y-(y/6)*6+1;
+        return abs(r1-r2)+abs(c1-c2);
+    }
+    int help(int i,int f1,int f2,string &s)
+    {
+        if(i==s.length())
+            return 0;
+        if(dp[i][f1][f2]!=-1)
+            return dp[i][f1][f2];
+        return dp[i][f1][f2]=min(help(i+1,s[i]-'A',f2,s)+dist(s[i]-'A',f1),help(i+1,f1,s[i]-'A',s)+dist(s[i]-'A',f2));
+    }
+    int minimumDistance(string word) 
+    {
+        int res=INT_MAX;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<26;i++)
+        {
+            res=min(res,help(0,word[0]-'A',i,word));
+        }
+        return res;
+    }
+};
