160c02e6f7b6a69e99dfea0be607a73e346d20b8
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-14 11:54:10 +0530

    Time: 16 ms (40.00%), Space: 6 MB (60.00%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int dp[10][2][1<<10+1];
-    int help(int i,bool b,int cur,string &s)
+    int dp[10][2][1<<10];
+    int help(int i,bool b,int mask,string &s)
     {
         if(i==s.length())
             return 1;
-        if(dp[i][b][cur]!=-1)
-            return dp[i][b][cur];
+        if(dp[i][b][mask]!=-1)
+            return dp[i][b][mask];
         int ans=0;
         for(int digit=0;digit<=9;digit++)
         {
-            if((b or (s[i]-'0')>=digit) and (cur&(1<<digit))==0)
-                ans+=help(i+1,b|(s[i]-'0'>digit),cur|(cur<=1 and digit==0 ? 0:(1<<digit)),s);
+            int newMask=mask;
+            if(mask>1 or digit!=0)
+                newMask|=(1<<digit);
+            if((b or (s[i]-'0')>=digit) and (mask&(1<<digit))==0)
+                ans+=help(i+1,b|(s[i]-'0'>digit),newMask,s);
         }
-        return dp[i][b][cur]=ans;
+        return dp[i][b][mask]=ans;
     }
     int countSpecialNumbers(int n)
     {
