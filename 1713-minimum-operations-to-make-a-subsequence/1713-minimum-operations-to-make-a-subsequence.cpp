6ba7ed2085606aa912bbd0192e291208f2a6fd06
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-18 21:44:51 +0530

    Time: 618 ms (49.81%), Space: 109.9 MB (87.07%) - LeetHub

--- 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.cpp
+++ 1713-minimum-operations-to-make-a-subsequence/1713-minimum-operations-to-make-a-subsequence.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int minOperations(vector<int>& target, vector<int>& arr) {
         int n=arr.size(),m=target.size();
-        unordered_map<int,vector<int>> ind;
-        for(int i=0;i<n;i++)
-            ind[arr[i]].push_back(i);
+        unordered_map<int,int> ind;
         for(int i=0;i<m;i++)
         {
-            if(ind.count(target[i]))
-            {
-                for(auto &index:ind[target[i]])
-                    arr[index]=-i;
-            }
+            ind[target[i]]=i;
         }
         vector<int> lis;
         for(auto &num:arr)
         {
-            if(num>0)
+            if(ind.count(num)==0)
                 continue;
-            num*=-1;
+            num=ind[num];
             if(lis.size()==0)
                 lis.push_back(num);
             else if(num>lis.back())
