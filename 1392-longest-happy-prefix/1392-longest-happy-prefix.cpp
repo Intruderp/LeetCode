3df5a02b4375eaff994050d948b6bc9628078054
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-20 13:11:02 +0530

    Time: 54 ms (59.75%), Space: 14.8 MB (95.55%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -2,26 +2,16 @@ class Solution {
 public:
     string longestPrefix(string s) 
     {
-        long power=1,p=31,hash=0,hash2=0,mod=1e9+7,n=s.length(),len=0,p2=33,power2=1;
-        set<pair<int,int>> h;
-        for(char &c:s)
+        int n=s.length(),p=31,mod=1e9+7,len=0;
+        long prefixHash=0,suffixHash=0,power=1;
+        for(int i=0;i<n-1;i++)
         {
-            hash=(hash+(c-'a'+1)*power)%mod;
+            prefixHash=(prefixHash+(s[i]-'a'+1)*power)%mod;
+            suffixHash=(suffixHash*p)%mod;
+            suffixHash=(suffixHash+(s[n-i-1]-'a'+1))%mod;
             power=(power*p)%mod;
-            hash2=(hash2+(c-'a'+1)*power2)%mod;
-            power2=(power2*p2)%mod;
-            h.insert({hash,hash2});
-        }
-        hash=0;
-        hash2=0;
-        for(int i=n-1;i>0;i--)
-        {
-            hash=(hash*p)%mod;
-            hash=(hash+(s[i]-'a'+1))%mod;
-            hash2=(hash2*p2)%mod;
-            hash2=(hash2+(s[i]-'a'+1))%mod;
-            if(h.count({hash,hash2}))
-                len=n-i;
+            if(suffixHash==prefixHash)
+                len=i+1;
         }
         return s.substr(0,len);
     }
