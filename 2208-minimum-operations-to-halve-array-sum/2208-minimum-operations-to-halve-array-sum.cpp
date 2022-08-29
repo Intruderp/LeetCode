d3ba5e10f44ca329648ceb482f114f63dd85fcfa
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-08-29 13:00:52 +0530

    Time: 490 ms (26.80%), Space: 89.5 MB (32.32%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        priority_queue<double> pq;
+        int res=0;
+        long sum=accumulate(nums.begin(),nums.end(),0ll);
+        double curSum=sum;
+        for(auto &num:nums)
+            pq.push(num);
+        while(curSum*2>sum)
+        {
+            double t=pq.top();
+            pq.pop();
+            curSum-=t-t/2;
+            pq.push(t-t/2);
+            res++;
+        }
+        return res;
+    }
+};
