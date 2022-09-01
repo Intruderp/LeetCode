17a791ada30f7cfb2b6b8ed0cf2e36993829ad53
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-01 12:30:21 +0530

    Time: 10 ms (64.85%), Space: 8 MB (39.13%) - LeetHub

--- 649-dota2-senate/649-dota2-senate.cpp
+++ 649-dota2-senate/649-dota2-senate.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string predictPartyVictory(string senate)
     {
-        deque<int> R,D,temp,temp2;
+        deque<int> R,D;
         int n=senate.length(),x,y;
         for(int i=0;i<n;i++)
         {
@@ -13,46 +13,15 @@ public:
         }
         while(R.size()>0 and D.size()>0)
         {
-            while(R.size()>0 or D.size()>0)
-            {
-                if(D.size()==0)
-                {
-                    if(temp2.size())
-                        temp2.pop_front();
-                    else
-                        break;
-                    temp.push_back(R.front());
-                    R.pop_front();
-                }
-                else if(R.size()==0)
-                {
-                    if(temp.size())
-                        temp.pop_front();
-                    else
-                        break;
-                    temp2.push_back(D.front());
-                    D.pop_front();
-                }
-                else
-                {
-                    x=R.front(),y=D.front();
-                    D.pop_front();
-                    R.pop_front();
-                    if(x<y)
-                    {
-                        temp.push_back(x);
-                    }
-                    else
-                    {
-                        temp2.push_back(y);
-                    }
-                        
-                }
-            }
-            R=temp;
-            D=temp2;
-            temp.clear();
-            temp2.clear();
+            x=R.front();
+            y=D.front();
+            R.pop_front();
+            D.pop_front();
+            if(x<y)
+                R.push_back(x+n);
+            else
+                D.push_back(y+n);
+                
         }
         return D.size()>0 ? "Dire":"Radiant";
     }
