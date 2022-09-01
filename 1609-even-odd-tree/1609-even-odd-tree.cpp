4303e150cc747a352fc4211f7edf0552f28aa899
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-01 16:52:56 +0530

    Time: 548 ms (20.05%), Space: 150.9 MB (79.71%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) 
+    {
+        queue<TreeNode*> q;
+        q.push(root);
+        int level=0;
+        while(!q.empty())
+        {
+            int sz=q.size(),mini=INT_MAX,maxi=0;
+            while(sz--)
+            {
+                auto f=q.front();
+                q.pop();
+                if(level%2)
+                {
+                    if(f->val%2 or f->val>=mini)
+                        return false;
+                    mini=f->val;
+                }
+                else
+                {
+                    if(f->val%2==0 or f->val<=maxi)
+                        return false;
+                    maxi=f->val;
+                }
+                if(f->left!=NULL)
+                    q.push(f->left);
+                if(f->right!=NULL)
+                    q.push(f->right);
+            }
+            level++;
+        }
+        return true;
+    }
+};
