a3a02f883b26f4b4b8bdd212fc0a5e39bb2d1d22
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-03 22:40:24 +0530

    Time: 34 ms (100.00%), Space: 8.3 MB (100.00%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void help(int j,int cols,int &n,int &m,int &res,vector<int> &taken,vector<vector<int>> &mat)
+    void help(int j,int cols,int &n,int &m,int &res,int mask,vector<vector<int>> &mat)
     {
         if(cols<0)
             return ;
@@ -12,7 +12,7 @@ public:
                 bool poss=true;
                 for(int j=0;j<m;j++)
                 {
-                    if(mat[i][j] and !taken[j])
+                    if(mat[i][j] and (mask&(1<<j))==0)
                     {
                         poss=false;
                         break;
@@ -23,16 +23,13 @@ public:
             res=max(res,cnt);
             return ;
         }
-        taken[j]=1;
-        help(j+1,cols-1,n,m,res,taken,mat);
-        taken[j]=0;
-        help(j+1,cols,n,m,res,taken,mat);
+        help(j+1,cols-1,n,m,res,mask|(1<<j),mat);
+        help(j+1,cols,n,m,res,mask,mat);
     }
     int maximumRows(vector<vector<int>>& mat, int cols) 
     {
         int res=0,n=mat.size(),m=mat[0].size();
-        vector<int> taken(m,0);
-        help(0,cols,n,m,res,taken,mat);
+        help(0,cols,n,m,res,0,mat);
         return res;
     }
 };
