745216301f37a3c59536375b6995b67c19c2bb8b
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-04 11:01:03 +0530

    Time: 136 ms (66.67%), Space: 57.1 MB (100.00%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
+    bool check(int i,int j,vector<int> &nums)
+    {
+        for(int x=i;x<=j;x++)
+        {
+            for(int y=x+1;y<=j;y++)
+            {
+                if(nums[x]&nums[y])
+                    return false;
+            }
+        }
+        return true;
+    }
     int longestNiceSubarray(vector<int>& nums) {
         int res=1,n=nums.size();
         for(int i=0;i<n;i++)
         {
-            vector<int> bit(32,0);
-            bool poss=true;
-            for(int j=i;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
-               for(int b=0;b<32;b++)
-               {
-                   if((1<<b)&nums[j])
-                       bit[b]++;
-                   if(bit[b]>1)
-                   {
-                       poss=false;
-                       break;
-                   }
-               }
-               if(poss)
-                   res=max(res,j-i+1);
-                else
+                if((nums[i]&nums[j])==0 and check(i,j,nums))
+                    res=max(res,j-i+1);
+                else 
                     break;
             }
         }
