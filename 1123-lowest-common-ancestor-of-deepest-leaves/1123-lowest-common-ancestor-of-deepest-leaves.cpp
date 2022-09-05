920400d72fbc8e8f07ef4ffe6792458004d2a5e3
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-05 17:44:27 +0530

    Time: 26 ms (45.28%), Space: 21.2 MB (68.40%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,49 +11,23 @@
  */
 class Solution {
 public:
-    int maxDepth(TreeNode* root)
+    int help(TreeNode* node,int depth,int &maxDepth,TreeNode* &lca)
     {
-        if(root==NULL)
-            return 0;
-        return max(maxDepth(root->left),maxDepth(root->right))+1;
-    }
-    void count(TreeNode* root,int level,int &depth,int &cnt)
-    {
-        if(root==NULL)
-            return ;
-        if(depth==level+1)
-            cnt++;
-        count(root->left,level+1,depth,cnt);
-        count(root->right,level+1,depth,cnt);
-    }
-    int help(TreeNode* root,TreeNode* &res,int level,int &cnt,int &depth,int &maxi)
-    {
-        if(root==NULL)
-            return 0;
-        if(depth==level+1)
+        maxDepth=max(maxDepth,depth);
+        if(node==NULL)
+            return depth;
+        int left=help(node->left,depth+1,maxDepth,lca);
+        int right=help(node->right,depth+1,maxDepth,lca);
+        if(right==left and right==maxDepth)       // this is a potential answer as it contain deepest node on both sides 
         {
-            if(cnt==1)
-            {
-                maxi=level;
-                res=root;
-            }
-            return 1;
+            lca=node;               // this will be updated only if any node above this node also satisfy the condition
         }
-        int deepestLeavesCnt=help(root->left,res,level+1,cnt,depth,maxi)+help(root->right,res,level+1,cnt,depth,maxi);
-        if(deepestLeavesCnt==cnt and level>maxi)
-        {
-            maxi=level;
-            res=root;
-        }
-        return deepestLeavesCnt;
-            
+        return max(left,right);
     }
-    TreeNode* lcaDeepestLeaves(TreeNode* root) 
-    {
-        int depth=maxDepth(root),cnt=0,maxi=-1;
-        count(root,0,depth,cnt);
-        TreeNode* res=NULL;
-        help(root,res,0,cnt,depth,maxi);
-        return res;
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        TreeNode* lca=NULL;
+        int maxDepth=0;
+        help(root,0,maxDepth,lca);
+        return lca;
     }
 };
