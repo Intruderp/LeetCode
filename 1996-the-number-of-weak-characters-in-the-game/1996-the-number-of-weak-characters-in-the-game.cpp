bdf2fb7d3e139191e36a3b9d3f4c115912a8bf78
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-09 12:59:36 +0530

    Time: 1471 ms (15.82%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a,vector<int> &b)
-    {
-        if(a[0]==b[0])
-            return a[1]>b[1];
-        return a[0]<b[0];
-    }
     int numberOfWeakCharacters(vector<vector<int>>& properties) 
     {
         int weakCharacters=0,n=properties.size(),maxDefense=0;
-        sort(properties.begin(),properties.end(),comp);
+        for(auto &property:properties)
+            property[1]*=-1;
+        sort(properties.begin(),properties.end());
         for(int i=n-2;i>=0;i--)
         {
             if(properties[i+1][0]!=properties[i][0])
-                maxDefense=max(maxDefense,properties[i+1][1]);
-            if(maxDefense>properties[i][1])
+                maxDefense=max(maxDefense,-properties[i+1][1]);
+            if(maxDefense>-properties[i][1])
                 weakCharacters++;
         }
         return weakCharacters;
