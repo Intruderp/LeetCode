d08c6ff6842963c51a23e7a07650220616d91e91
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-09 12:58:10 +0530

    Time: 1604 ms (7.08%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int numberOfWeakCharacters(vector<vector<int>>& p) 
+    static bool comp(vector<int> &a,vector<int> &b)
     {
-        int ans=0,mx=0,n=p.size(),x,y,z;
-        vector<pair<int,int>> a;
-        for(int i=0;i<n;i++)
-            a.push_back({p[i][0],p[i][1]});
-        int suff[n];
-        sort(a.begin(),a.end());
-        for(int i=n-1;i>=0;i--)
+        if(a[0]==b[0])
+            return a[1]>b[1];
+        return a[0]<b[0];
+    }
+    int numberOfWeakCharacters(vector<vector<int>>& properties) 
+    {
+        int weakCharacters=0,n=properties.size(),maxDefense=0;
+        sort(properties.begin(),properties.end(),comp);
+        for(int i=n-2;i>=0;i--)
         {
-            mx=max(mx,a[i].second);
-            suff[i]=mx;
+            if(properties[i+1][0]!=properties[i][0])
+                maxDefense=max(maxDefense,properties[i+1][1]);
+            if(maxDefense>properties[i][1])
+                weakCharacters++;
         }
-        for(int i=0;i<n;i++)
-        {
-            x=a[i].first,y=a[i].second;
-            pair<int,int> pp={x+1,0};
-            auto it=lower_bound(a.begin(),a.end(),pp);
-            if(it!=a.end())
-            {
-                z=it-a.begin();
-                ans+=(suff[z]>y);
-            }
-        }
-        return ans;
+        return weakCharacters;
     }
 };
