b68a45ef8f798006235b7263ae4a83bd25fc6980
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-09-25 12:56:41 +0530

    Time: 126 ms (23.28%), Space: 17.4 MB (98.69%) - LeetHub

--- 757-set-intersection-size-at-least-two/757-set-intersection-size-at-least-two.cpp
+++ 757-set-intersection-size-at-least-two/757-set-intersection-size-at-least-two.cpp
@@ -7,32 +7,38 @@ public:
     int intersectionSizeTwo(vector<vector<int>>& intervals) 
     {
         sort(intervals.begin(),intervals.end(),comp);
-        set<int> s;
-        int req=0;
+        int prev=-1,prev2=-1;
+        int req=0,minSize=0;
         for(auto &interval:intervals)
         {
             req=2;
-            auto it=s.lower_bound(interval[0]);
-            if(it!=s.end() and *it<=interval[1])
+            if(prev==-1)
             {
-                req--;
-                it++;
-                if(it!=s.end() and *it<=interval[1])
-                req--;
+                prev=interval[1]-1;
+                prev2=interval[1];
+            }
+            else
+            {
+                if(prev>=interval[0])
+                    req--;
+                if(prev2>=interval[0])
+                    req--;
             }
             if(req==1)
             {
-                if(s.count(interval[1]))
-                    s.insert(interval[1]-1);
+                prev=prev2;
+                if(prev2!=interval[1])
+                    prev2=interval[1];
                 else
-                    s.insert(interval[1]);
+                    prev2=interval[1]-1;
             }
             else if(req==2)
             {
-                s.insert(interval[1]-1);
-                s.insert(interval[1]);
+                prev=interval[1]-1;
+                prev2=interval[1];
             }
+             minSize+=req;
         }
-        return s.size();
+        return minSize;
     }
 };
