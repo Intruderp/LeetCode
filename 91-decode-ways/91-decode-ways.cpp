1fc35140d0763f5d86b57989aa366d7a1ad4c23e
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-10-01 07:55:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -22,7 +22,40 @@ public:
     }
     int numDecodings(string s)
     {
-        memset(dp,-1,sizeof(dp));
-        return help(0,s);
+        int n=s.length();
+        if(s[0]=='0')
+            return 0;
+        if(n==1)
+            return 1;
+        vector<int> dp(n,0);
+        dp[0]=1;
+        if(s[1]!='0')
+        {
+            dp[1]=1+poss(s[0],s[1]);
+        }
+        else
+        {
+            dp[1]=poss(s[0],s[1]);
+        }
+        if(dp[1]==0)
+            return 0;
+        for(int i=2;i<n;i++)
+        {
+            if(s[i]=='0')
+            {
+                if(s[i-1]!='0' and poss(s[i-1],'0'))
+                    dp[i]=dp[i-2];
+            }
+            else
+            {
+                dp[i]=dp[i-1];
+                if(s[i-1]!='0' and poss(s[i-1],s[i]))
+                    dp[i]+=dp[i-2];
+            }
+            if(dp[i]==0)
+                return 0;
+        }
+        return dp[n-1];
     }
+    
 };
