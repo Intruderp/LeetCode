b69a22554c6f7ee52217ff0334a2eff3ab913b46
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-10-01 07:56:52 +0530

    Time: 3 ms (63.51%), Space: 6.1 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    int dp[100];
     bool poss(char c1,char c2)
     {
         int x=(c1-'0')*10+(c2-'0');
         return x<=26;
-    }
-    int help(int i,string &s)
-    {
-        if(i==s.length())
-            return 1;
-        if(s[i]=='0')
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int ans=0;
-        ans+=help(i+1,s);
-        if(i+1<s.length() and poss(s[i],s[i+1]))
-            ans+=help(i+2,s);
-        return dp[i]=ans;
     }
     int numDecodings(string s)
     {
@@ -37,8 +22,6 @@ public:
         {
             dp[1]=poss(s[0],s[1]);
         }
-        if(dp[1]==0)
-            return 0;
         for(int i=2;i<n;i++)
         {
             if(s[i]=='0')
@@ -52,8 +35,6 @@ public:
                 if(s[i-1]!='0' and poss(s[i-1],s[i]))
                     dp[i]+=dp[i-2];
             }
-            if(dp[i]==0)
-                return 0;
         }
         return dp[n-1];
     }
