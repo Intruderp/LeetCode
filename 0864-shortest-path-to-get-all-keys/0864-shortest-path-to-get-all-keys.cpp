5306ca62350d3b92cf8dacb979d4edb20da7b65e
Intruderp <86315404+Intruderp@users.noreply.github.com>
2022-10-12 12:56:25 +0530

    Time: 230 ms (47.37%), Space: 28.5 MB (37.46%) - LeetHub

--- 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
+++ 0864-shortest-path-to-get-all-keys/0864-shortest-path-to-get-all-keys.cpp
@@ -1,96 +1,57 @@
 class Solution {
 public:
-    int n,m,x,y,res=INT_MAX;
+    int n,m;
     vector<int> dir{1,0,-1,0,1};
     bool valid(int &i,int &j)
     {
         return i>=0 and i<n and j>=0 and j<m;
     }
-    void fill(vector<vector<int>> &dist,int x)
-    {
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-                dist[i][j]=x;
-        }
-    }
-    int help(vector<vector<int>> &keys,vector<string>&grid)
-    {
-        vector<vector<int>> dist(n,vector<int>(m,-1));
-        dist[x][y]=0;
-        queue<pair<int,int>> q;
-        q.push({x,y});
-        int k=0,ans=0;
-        unordered_set<char> lock;
-        while(!q.empty())
-        {
-            auto f=q.front();
-            q.pop();
-            for(int d=0;d<4;d++)
-            {
-                int i=f.first+dir[d],j=f.second+dir[d+1];
-                if(valid(i,j) and dist[i][j]==-1 and grid[i][j]!='#')
-                {
-                    if(grid[i][j]>='a' and grid[i][j]<='z')
-                    {
-                        if(keys[k][0]==i and keys[k][1]==j)
-                        {
-                            dist[i][j]=1+dist[f.first][f.second];
-                            ans+=dist[i][j];
-                            k++;
-                            if(k==keys.size() or ans>res)
-                                return ans;
-                            lock.insert(toupper(grid[i][j]));
-                            fill(dist,-1);
-                            queue<pair<int,int>> temp;
-                            dist[i][j]=0;
-                            temp.push({i,j});
-                            q=temp;
-                            break;
-                        }
-                        else if(lock.count(toupper(grid[i][j])))
-                        {
-                            dist[i][j]=1+dist[f.first][f.second];
-                            q.push({i,j});
-                        }
-                    }
-                    else if(grid[i][j]>='A' and grid[i][j]<='Z')
-                    {
-                        if(lock.count(grid[i][j]))
-                        {
-                            dist[i][j]=1+dist[f.first][f.second];
-                            q.push({i,j});
-                        }
-                    }
-                    else if(grid[i][j]=='.' or grid[i][j]=='@')
-                    {
-                        dist[i][j]=1+dist[f.first][f.second];
-                        q.push({i,j});
-                    }
-                }
-            }
-        }
-        return INT_MAX;
-    }
     int shortestPathAllKeys(vector<string>& grid) 
     {
         n=grid.size(),m=grid[0].size();
-        vector<vector<int>> keys;
+        int level=0,key=0,x,y;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='@')
                     x=i,y=j;
-                else if(grid[i][j]>='a' and grid[i][j]<='z')
-                    keys.push_back({i,j});
+                else if(grid[i][j]>='a' and grid[i][j]<='f')
+                    key++;
             }
         }
-        sort(keys.begin(),keys.end());
-        do
+        vector<vector<vector<int>>> vis(n,vector<vector<int>>(m,vector<int>((1<<key),0)));
+        queue<vector<int>> q;
+        q.push({x,y,0});
+        vis[x][y][0]=1;
+        while(!q.empty())
         {
-            res=min(res,help(keys,grid));
-        }while(next_permutation(keys.begin(),keys.end()));
-        return res==INT_MAX ? -1:res;
+            int sz=q.size();
+            while(sz--)
+            {
+                auto f=q.front();
+                q.pop();
+                for(int d=0;d<4;d++)
+                {
+                    x=f[0]+dir[d],y=f[1]+dir[d+1];
+                    if(!valid(x,y) or grid[x][y]=='#')
+                        continue;
+                    if(grid[x][y]>='A' and grid[x][y]<='F' and (f[2]&(1<<(grid[x][y]-'A')))==0)
+                        continue;
+                    int newMask=f[2];
+                    if(grid[x][y]>='a' and grid[x][y]<='f')
+                        newMask|=(1<<(grid[x][y]-'a'));
+                    if(newMask==((1<<key)-1))
+                       return level+1;
+                    if(vis[x][y][newMask]==0)
+                    {
+                        q.push({x,y,newMask});
+                        vis[x][y][newMask]=1;
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
     }
 };
